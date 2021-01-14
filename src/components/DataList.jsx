4988846f22ffae8697304e51b3313fe74bc2f15c
neda-jahadi <neda_jahadi@yahoo.com>
2021-01-14 09:36:14 +0100

    Rensade i DataList komponent och tog bort upprepande koder

--- src/components/DataList.jsx
+++ src/components/DataList.jsx
@@ -3,30 +3,36 @@ import './DataList.css';
 import deleteIcon from '../assets/delete.png';
 import editIcon from '../assets/edit.png';
 import backgroundImg from '../assets/background.jpg';
-
+import {useHistory} from 'react-router-dom';
 
 const DataList = ({ data, source, id1, updateCity }) => {
+    const history = useHistory();
     const [like, setLike] = useState(false);
+    let deleteTitle = '', editTitle = '';
 
-    const OnDeleteActivity = (id2) => {
-        fetch(`http://localhost:2294/api/deleteactivity/?id1=${id1}&id2=${id2}`)
+    switch (source) {
+        case 'activity':
+            deleteTitle = 'deleteactivity';
+            editTitle = 'editactivity';
+            break;
+    
+        default:
+            deleteTitle = 'deleterestaurant';
+            editTitle = 'editrestaurant';
+            break;
+    }
+
+    const OnDeleteEntity = (id2) => {
+        fetch(`http://localhost:2294/api/${deleteTitle}/?id1=${id1}&id2=${id2}`)
             .then(response => response.text())
             .then(res => console.log(res))
             .catch(error => console.log(error))
         updateCity();
     }
 
-    const OnDeleteRestaurant = (id2) => {
-        fetch(`http://localhost:2294/api/deleterestaurant/?id1=${id1}&id2=${id2}`)
-            .then(response => response.text())
-            .then(res => console.log(res))
-            .catch(error => console.log(error))
-        updateCity();
-    }
+    
 
-    let dataList = null;
-    if (source === 'activity') {
-            dataList = data.map((datum, index) => <div className="activity-box" key={index}>
+    let dataList = data.map((datum, index) => <div className="activity-box" key={index}>
             <div className="img-holder">
                 <img src={backgroundImg} alt="backImg" className="backImg" />
 
@@ -57,55 +63,13 @@ const DataList = ({ data, source, id1, updateCity }) => {
                     : <div className="liked" onClick={() => setLike(false)}>Liked </div>}
 
                 <div className="Delete-edit">
-                    <img src={deleteIcon} alt="delete" className="delete-icon" onClick={() => OnDeleteActivity(index)} />
-                    <img src={editIcon} alt="edit"  />
+                    <img src={deleteIcon} alt="delete" className="delete-icon" onClick={() => OnDeleteEntity(index)} />
+                    <img src={editIcon} alt="edit" onClick={() =>   history.push(`/add/${editTitle}/${index}`)} />
                 </div>
 
             </div>
 
         </div>)
-     }else{
-            dataList = data.map((datum, index) => <div className="activity-box" key={index}>
-            <div className="img-holder">
-                <img src={backgroundImg} alt="backImg" className="backImg" />
-
-            </div>
-            <div className="activity-holder">
-                <div>
-                    <div className="title">Name:</div>
-                    <div className="item-info">{datum.name}</div>
-                </div>
-                <div>
-                    <div className="title">Address:</div>
-                    <div className="item-info">{datum.address}</div>
-                </div>
-                <div>
-                    <div className="title">Likes: </div>
-                    <div className="item-info">{datum.likes}</div>
-                </div>
-                <div>
-                    <div className="title">Comment: </div>
-                    <div className="item-info">{datum.comment}</div>
-                </div>
-
-            </div>
-
-            <div className="iconForItem-holder">
-                {!like
-                    ? <div className="like" onClick={() => setLike(true)}>Like </div>
-                    : <div className="liked" onClick={() => setLike(false)}>Liked </div>}
-
-                <div className="Delete-edit">
-                    <img src={deleteIcon} alt="delete" className="delete-icon" onClick={() => OnDeleteRestaurant(index)} />
-                    <img src={editIcon} alt="edit" />
-                </div>
-
-            </div>
-
-        </div>)
-     }
-        
-         
     
 
 return (
