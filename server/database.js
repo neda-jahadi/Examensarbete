58f0a6f17f8af8dd9e5668fb6f9881c04c6be6ce
neda-jahadi <neda_jahadi@yahoo.com>
2021-02-04 09:54:18 +0100

    röd border för invalid input fält

--- server/database.js
+++ server/database.js
@@ -1,6 +1,6 @@
 const { MongoClient, ObjectID } = require('mongodb')
-// const url = 'mongodb+srv://neda:7713601nj@cluster0.pn5gs.mongodb.net/goteborgaren_bera?retryWrites=true&w=majority';
-const url = 'mongodb://localhost:27017';
+const url = 'mongodb+srv://nedajahadi:FS0qRFd6leqrnjyx@examensarbete.psbpx.mongodb.net/trip?retryWrites=true&w=majority';
+// const url = 'mongodb://localhost:27017';
 const dbName = 'trip';
 const cityCollectionName = 'cities';
 const userCollectionName = 'users';
